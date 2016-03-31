6ff7151c8276740500b53fb878c0baef3da9217e
Magistrazh <1122123@mail.ru>
2016-03-31 16:16:19 +0300

    StandardRankStrategy реализован фунционал перобразования из ProblemResultInfo в TotalResultInfo

--- TestSystem/MonitoringModule/src/main/java/com/netcracker/monitoring/rank/StandardRankStrategy.java
+++ TestSystem/MonitoringModule/src/main/java/com/netcracker/monitoring/rank/StandardRankStrategy.java
@@ -7,13 +7,58 @@ package com.netcracker.monitoring.rank;
 
 import com.netcracker.monitoring.info.ProblemResultInfo;
 import com.netcracker.monitoring.info.TotalResultInfo;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
  * @author Магистраж
  */
-public class StandardRankStrategy{
+public class StandardRankStrategy implements RankStrategy {
 
+    @Override
+    public List<TotalResultInfo> formResults(List<ProblemResultInfo> problemResultInfos) {
+        List<TotalResultInfo> resultInfoList = new ArrayList<>();
+        Map<Integer, List<ProblemResultInfo>> idToProblemResultMap = new HashMap<>();
+
+        for (ProblemResultInfo info : problemResultInfos) {
+
+            if (!idToProblemResultMap.containsKey(info.getUserId())) {
+                idToProblemResultMap.put(info.getUserId(), new ArrayList<>());
+            }
+            idToProblemResultMap.get(info.getUserId()).add(info);
+        }
+        for (Map.Entry<Integer, List<ProblemResultInfo>> entry : idToProblemResultMap.entrySet()) {
+            resultInfoList.add(new TotalResultInfo(entry.getKey(), entry.getValue()));
+        }
+
+        Collections.sort(resultInfoList, new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                TotalResultInfo obj1 = (TotalResultInfo) o1;
+                TotalResultInfo obj2 = (TotalResultInfo) o2;
+
+                if (obj1.getPoints() > obj2.getPoints()) {
+                    return -1;
+                } else if (obj2.getPoints() > obj1.getPoints()) {
+                    return 1;
+                } else {
+                    return obj1.getFine() < obj2.getFine() ? -1 : 1;
+                }
+            }
+        });
+
+        for (int i = 0; i < resultInfoList.size(); i++) {
+            resultInfoList.get(i).setPlace(i + 1);
+
+        }
+
+        return resultInfoList;
+    }
 
 }
