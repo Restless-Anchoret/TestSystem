67c97b2ffe4601bb4fb21779e0ca1eeb5e8b3ac1
RestlessAnchoret <et.anchoret@gmail.com>
2016-05-14 13:57:10 +0200

    Страница с кодом посылки доделана

--- TestSystem/WebModule/src/main/java/com/netcracker/web/participants/SubmissionCodeController.java
+++ TestSystem/WebModule/src/main/java/com/netcracker/web/participants/SubmissionCodeController.java
@@ -2,44 +2,55 @@ package com.netcracker.web.participants;
 
 import com.netcracker.businesslogic.holding.SubmissionCodeEJB;
 import com.netcracker.database.dal.SubmissionFacadeLocal;
+import com.netcracker.database.entity.Submission;
 import com.netcracker.web.logging.WebLogging;
+import com.netcracker.web.util.JSFUtil;
 import java.io.IOException;
 import java.util.logging.Level;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpServletRequest;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
 
+@Named
+@RequestScoped
 public class SubmissionCodeController {
 
     @EJB(beanName = "SubmissionCodeEJB")
     private SubmissionCodeEJB submissionCodeEJB;
-    private Integer submissionId;
+    @EJB(beanName = "SubmissionFacade")
+    private SubmissionFacadeLocal submissionFacade;
+    private Integer id;
+    private Submission submission;
     private String submissionText;
 
     @PostConstruct
     public void initSubmissionPage() {
         WebLogging.logger.log(Level.INFO, "submission page");
-        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
-        Integer id;
+        Integer tmpId;
         try {
-            String strId = request.getParameter("submissionId");
-            WebLogging.logger.log(Level.SEVERE, "submissionId {0}", strId);
+            String strId = JSFUtil.getRequestParameter("id");
+            WebLogging.logger.log(Level.SEVERE, "id {0}", strId);
             if (strId == null) {
                 WebLogging.logger.log(Level.SEVERE, "submissionId is null");
                 return;
             }
-            id = Integer.parseInt(strId);
+            tmpId = Integer.parseInt(strId);
         } catch (Throwable ex) {
             WebLogging.logger.log(Level.SEVERE, null, ex);
             return;
         }
-        submissionId = id;
+        id = tmpId;
+        submission = submissionFacade.find(id);
         try {
-            submissionText = submissionCodeEJB.getSubmissionCode(submissionId);
+            submissionText = submissionCodeEJB.getSubmissionCode(submission);
         } catch (IOException ex) {
             WebLogging.logger.log(Level.SEVERE, null, ex);
         }
+    }
+
+    public Integer getId() {
+        return id;
     }
 
     public String getSubmissionText() {
